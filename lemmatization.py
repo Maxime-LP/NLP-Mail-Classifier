89d9e30331aedcf769c07eaca9ac8c2f6ed5c3e4
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2022-02-10 22:33:35 +0100

    model and lemmatizer updates

--- lemmatization.py
+++ lemmatization.py
@@ -3,11 +3,9 @@ from collections import Counter, defaultdict
 import pandas as pd
 import os, spacy, re
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
 from utilities import progressbar
-tokenizer = HashingVectorizer(ngram_range = (1,2)).build_tokenizer()
+from unidecode import unidecode
 
-############################################################################
 def initSpacy(spacyModule):
     try:
         lemmatizer = spacy.load(spacyModule, disable = ['parser', 'attribute_ruler', 'ner'])
@@ -17,32 +15,25 @@ def initSpacy(spacyModule):
         lemmatizer = spacy.load(spacyModule)
     return lemmatizer
 
-def lemmatizer(inputText, spacynlp):    
-    doc = spacynlp(inputText)
-    words_lemmas_list = [token.lemma_.lower() for token in doc if not token.is_stop and re.match('^[a-z]+$', token.lemma_.lower()) is not None]
+def lemmatizeText(inputText, lemmatizer):    
+    doc = lemmatizer(inputText)
+    words_lemmas_list = [unidecode(token.lemma_.lower()) for token in doc if not token.is_stop and re.match('^\w+$', token.lemma_.lower()) is not None]
     outputDict = dict(Counter(words_lemmas_list))
     
-    newText = ' '.join(words_lemmas_list)
-    ngrams_counter = nGrams(newText, nrange = (2,2))
-    outputDict.update(ngrams_counter)
+    #newText = ' '.join(words_lemmas_list)
+    #ngrams_counter = nGrams(newText, nrange = (2,2))
+    #outputDict.update(ngrams_counter)
     return outputDict
-
-def nGrams(inputText, nrange = (2,2)):
-    inputList = list(inputText)
-    Vect = CountVectorizer(analyzer='char_wb',encoding='utf-16',strip_accents='unicode',ngram_range=nrange)
-    X = Vect.fit(inputList)
-    return {key:1 for key in Vect.get_feature_names()}
-############################################################################
 
 def lemmatizeDF(df):
     tokens = defaultdict(list)
     totalSteps = len(df.text)
-    spacynlp = initSpacy('fr_core_news_md')
+    lemmatizer = initSpacy('fr_core_news_md')
     progressbar(0,totalSteps)
 
     for step,rawText in enumerate(df.text):
         if type(rawText)==str:
-            for word,count in lemmatizer(rawText, spacynlp).items():
+            for word,count in lemmatizeText(rawText, lemmatizer).items():
                 tokens[word] += [0]*(step-len(tokens[word])) + [count]
             progressbar(step+1,totalSteps)
 
