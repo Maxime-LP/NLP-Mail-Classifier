4a84844a566d675b6747df8a942c9ea9bfdb16b9
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2022-03-07 17:39:44 +0100

    Add files via upload

---
+++ onehotencoder.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from collections import Counter, defaultdict
+import pandas as pd
+import os, spacy, re
+from sklearn.feature_extraction.text import CountVectorizer
+import tqdm
+from unidecode import unidecode
+
+def initSpacy(spacyModule):
+    try:
+        lemmatizer = spacy.load(spacyModule, disable = ['parser', 'attribute_ruler', 'ner'])
+    except OSError:
+        print('Module introuvable. Téléchargement en cours ...')
+        os.system(f'python -m spacy download {spacyModule} >nul 2>&1')
+        lemmatizer = spacy.load(spacyModule)
+    return lemmatizer
+
+def countLemmas(inputText, lemmatizer):    
+    doc = lemmatizer(inputText)
+    words_lemmas_list = [unidecode(token.lemma_.lower()) for token in doc if not token.is_stop and re.match('^\w{2,}$', token.lemma_.lower()) is not None]
+    outputDict = dict(Counter(words_lemmas_list))
+    
+    #bigrams_list = [ words_lemmas_list[i] + " " + words_lemmas_list[i+1] for i in range(0, len(words_lemmas_list)-1)]
+    #outputDict.update(dict(Counter(bigrams_list)))
+
+    return outputDict
+
+def ooe_df(input_df):
+    tokens = defaultdict(list)
+    df = input_df['label'].copy()
+    df['text'] = input_df[['header', 'body']].astype(str).agg(' '.join, axis=1)
+
+    lemmatizer = initSpacy('fr_core_news_md')
+
+    for step,rawText in enumerate(df.text):
+        if type(rawText)==str:
+            for word,count in countLemmas(rawText, lemmatizer).items():
+                tokens[word] += [0]*(step-len(tokens[word])) + [count]
+
+    for word in tokens.keys():
+        tokens[word] += [0]*(step+1-len(tokens[word]))
+    
+    output_df = pd.DataFrame.from_dict(tokens,dtype=int)
+    output_df['demande_de_support'] = df['label']
+
+    return output_df
--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,8 @@
 import os, re  
 from collections import defaultdict
 import pandas as pd
-from lemmatization import lemmatizeDF
-from utilities import progressbar
 import extract_msg
+import multiprocessing as mp
 
 def TruncBody(message):
     messageBodyList = message.body.splitlines(keepends=False)
@@ -19,6 +18,21 @@ def TruncBody(message):
         i += 1
     return ' '.join(messageBodyList)
 
+def ClearText(txt):
+    #Removes boxes
+    txt = re.sub("\[.*?\]", "", txt) #non-greedy matches
+    txt = re.sub("<.*?>", "", txt)
+    #Removes link markers and punctuation
+    txt = re.sub("https?://|www", "", txt)
+    #txt = re.sub("[\\\.…\_,;!?@:/=\"«»><\(\)\[\]#}{~°+\|£$¤€*§%]", " ", txt)
+    txt = re.sub("([^\w\s]+)|(_+)", " ", txt)
+    #Removes isolated numbers
+    txt = re.sub(r"\b[0-9]+\b", "", txt)
+    #Replaces any stack of whitespaces with a single one
+    txt = re.sub("\s+", " ", txt)
+
+    return txt
+
 def isThisASupportMail(message, directory, sender):
     if directory == 'Non assistance' or message.subject.lower()[:3] == 're:' or message.subject.lower()[:4] == 're :' or \
     message.subject.lower()[:5] == '[toc]' or sender in ['chable brice', 'favreliere david', 'plonquet nadège', 'courtais yohan', 'le paumier maxime']:
@@ -28,82 +42,62 @@ def isThisASupportMail(message, directory, sender):
     else:
         return 2
 
-def ReadMail(message,directory):
+def ExtractInfo(filepath):
+    message = extract_msg.Message(filepath, delayAttachments = True)
+    directory = filepath.split('\\')[-2]
     mail = defaultdict(str)
+
+    mail['body'] = TruncBody(message).lower()
+    mail['body'] = ClearText(mail['body'])
+    mail['header'] = ClearText(message.subject.lower())
+
+    date = message.date
+    if date is not None:
+        mail['date'] = date
+    else:
+        mail['date'] = ''
+
     sender = re.search('<(.+)@',str(message.sender.lower()))
     if sender is None:
-        mail['sender'] = message.sender.lower()
+        mail['from'] = message.sender.lower()
     else:
         splits = sender.group(1).split('.')
-        mail['sender'] = ' '.join([splits[-1]] + splits[0:-1])
+        mail['from'] = ' '.join([splits[-1]] + splits[0:-1])
 
-    mail['demande_de_support'] = isThisASupportMail(message, directory, mail['sender'])
-
-    mail['text'] = message.subject.lower() + ' ' + TruncBody(message).lower()
-
-    #Removes boxes
-    mail['text'] = re.sub("\[.*?\]", "", mail['text']) #non-greedy matches
-    mail['text'] = re.sub("<.*?>", "", mail['text'])
-
-    #Removes link markers and punctuation
-    mail['text'] = re.sub("https?://|www", "", mail['text'])
-    mail['text'] = re.sub("[\\\.…\_,;!?@:/=\"«»><\(\)\[\]#}{~°+\|£$¤€*§%]", " ", mail['text'])
-
-    #Removes isolated numbers
-    mail['text'] = re.sub(r"\b[0-9]+\b", "", mail['text'])
-
-    #Replaces any stack of whitespaces with a single one
-    mail['text'] = re.sub("\s+", " ", mail['text'])
-
+    rec = message.recipients
+    if rec is not None:
+        mail['to'] = ''
+    else:
+        mail['to'] = ''
+    
+    mail['label'] = isThisASupportMail(message, directory, mail['from'])
     return mail
 
-def Parser(path):
-    #path > Assistance / Non assistance
-    df = pd.DataFrame(data=None,columns=['demande_de_support','sender','text'])
-    mails_dropped = []
+def ReadMail(filepath):
+    try:
+        return ExtractInfo(filepath)
+    except KeyboardInterrupt:
+        exit()
+    except Exception as e:
+        print(e," : mail ignoré")
+
+def Parser(path, writeto):
+    #path variable is the path to the mail folder
     print('PATH : ',path)
-    totalSteps, step = 3300, 0
-    progressbar(step,totalSteps)
-    for root,dirs,files in os.walk(path):
-        for file in files:
-            try:
-                directory = root.split('\\')[-1]
-                message = extract_msg.Message(os.path.join(root,file))
-                currentMail = ReadMail(message,directory)
-                df = df.append(currentMail,ignore_index=True)
-            except KeyboardInterrupt:
-                exit()
-            except Exception as e:
-                print(e," : mail ignoré")
-                mails_dropped.append(file)
-            step += 1
-            progressbar(step,totalSteps)
+    filesList = [os.path.join(root,file) for root, dir, files in os.walk(path) for file in files]
+    with mp.Pool(os.cpu_count()) as p:
+        mails = p.map(ReadMail, filesList)
+    df = pd.DataFrame(mails)
 
-    print('\n\n', len(mails_dropped), 'mails ignorés :', mails_dropped)
-    return df
+    with open(writeto,'w', encoding='utf-16') as csvfile:
+        df.to_csv(csvfile, encoding='utf-16')
 
-def writeFirstDataset(path,output):
-    #path variable is the path to the mail folders
-    df = Parser(path)
-    with open(output,'w',encoding='utf-16') as csvfile:
-        df.to_csv(csvfile,sep='¤',quotechar='§')
-
-def WriteFinalDataset(inputFile,outputFile,path=os.getcwd()):
+def preprocessing(firstFile, secondFile, path = os.getcwd()):
     os.system('cls')
-    if not os.path.isfile(os.path.join(os.getcwd(),inputFile)):
-        print("Ecriture des données ...")
-        writeFirstDataset(path,inputFile)
-    
-    print(f"Lecture de {inputFile} ...")
-    data = pd.read_csv(inputFile,sep='¤',quotechar='§',encoding='UTF-16', engine='python',header=0,skipinitialspace=True,dtype=str)
-
-    print("Traitement des données ...")
-    output_data = lemmatizeDF(data)
-
-    print(f"Ecriture de {outputFile} ...")
-    output_data.to_csv(outputFile,encoding='UTF-16')
-
+    if not os.path.isfile(os.path.join(os.getcwd(), firstFile)):
+        print("Lecture des mails ...")
+        Parser(path, firstFile)
 
 if __name__ == '__main__':
     path = r"\\hm.dm.ad\hmdoc\Direction Technique Assurances\Central\MOA décisionnel\DECIBEL\Suivi\Automatisation\Mail"
-    WriteFinalDataset('firstdataset.csv', 'finaldataset.csv', path)
+    preprocessing('mails.csv', path)
