738f6c01c624398c67cb31168482b663800640cc
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2022-03-07 17:41:05 +0100

    Delete utilities.py

--- utilities.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-def progressbar(step,totalSteps, size=60):
-    x = int(size*step/totalSteps)
-    sys.stdout.flush()
-    sys.stdout.write("[%s%s] %i/%i\r" % ("#"*x, "."*(size-x), step, totalSteps))
