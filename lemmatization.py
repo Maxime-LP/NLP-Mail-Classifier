2e10d90b5709e23723e9efb2a005fdccbb84b8d5
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2022-03-16 16:04:19 +0100

    Add files via upload

--- lemmatization.py
+++ lemmatization.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from collections import Counter, defaultdict
-import pandas as pd
 import os, spacy, re
 from unidecode import unidecode
 
@@ -23,27 +21,3 @@ def lemmatizeDf(df, input_col='text'):
     lemmatizer = initSpacy('fr_core_news_md')
     df[input_col + '_lem'] = df[input_col].apply(lambda row: lemmatizeRow(row[input_col], lemmatizer))
     return df
-
-def countWordsSentence(sentence):
-        word_list = sentence.split(' ')
-        outputDict = dict(Counter(word_list))
-        return outputDict
-
-def ohe_df(input_df, input_col = 'text'):
-    #Apply one hot encoding to a lemmatized text dataframe
-    tokens = defaultdict(list)
-    df = input_df[['label']].copy()
-    df['text'] = input_df[input_col]
-
-    for step,text in enumerate(df[input_col]):
-        if type(text)==str:
-            for word,count in countWordsSentence(text).items():
-                tokens[word] += [0]*(step-len(tokens[word])) + [count]
-
-    for word in tokens.keys():
-        tokens[word] += [0]*(step+1-len(tokens[word]))
-    
-    output_df = pd.DataFrame.from_dict(tokens,dtype=int)
-    output_df['demande_de_support'] = df['label']
-
-    return output_df
